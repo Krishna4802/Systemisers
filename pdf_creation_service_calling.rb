e4a6a16739681d0ac76e927ab3755da48602e993
Krishna-HAT <139359113+Krishna4802@users.noreply.github.com>
2024-10-30 14:28:55 +0530

    Add files via upload

---
+++ pdf_creation_service_calling.rb
@@ -0,0 +1,6 @@
+require './app/lib/project_report/generate_loan_amortization_pdf'
+loan_amount = 100_000
+annual_interest_rate = 10
+loan_years = 5
+pdf_service = ProjectReport::GenerateLoanAmortizationPdf.new(loan_amount, annual_interest_rate, loan_years)
+output_path = pdf_service.generate_pdf("/Users/krishnaprasath/Desktop/Loan_Amortization_Report.pdf")
